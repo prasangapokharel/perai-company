6ec7a73bc0d748bee529483ee01a1e5901b58ddd
prasangapokharel <prasangaramanpokharel@gmail.com>
2026-05-22 23:53:16 +0545

    chore: Remove buggy SMM folder - focus on Perai backend

--- smm/include/user-layout-start.php
+++
@@ -1,5 +0,0 @@
-<?php
-// User Layout Start - Wrapper for new layout-header.php
-// This ensures backward compatibility for pages still including this file
-require_once __DIR__ . '/layout-header.php';
-?>
